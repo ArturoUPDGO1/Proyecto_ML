a8a813a98fb6f608f00fdfd64436053767ce6fa2
PVega17102 <paul.vega.17102@gmail.com>
2022-06-06 22:32:08 -0500

    xd

--- Proyecto_ML/FormConfig.Designer.cs
+++ Proyecto_ML/FormConfig.Designer.cs
@@ -114,6 +114,8 @@ namespace Proyecto_ML
             | System.Windows.Forms.AnchorStyles.Left)));
             this.txtFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtFilePath.Location = new System.Drawing.Point(50, 76);
+            this.txtFilePath.MaximumSize = new System.Drawing.Size(461, 35);
+            this.txtFilePath.MinimumSize = new System.Drawing.Size(461, 35);
             this.txtFilePath.Name = "txtFilePath";
             this.txtFilePath.Size = new System.Drawing.Size(461, 35);
             this.txtFilePath.TabIndex = 0;
